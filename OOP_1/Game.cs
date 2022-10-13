38706e45c92f99ade86f24ea24c764be3db01acf
Sapogek <mrrexik1096@gmail.com>
2022-10-13 23:22:52 +0300

    Create Game.cs

---
+++ OOP_1/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_1
+{
+    public class Game
+    {
+        public enum ResultGame
+        {
+            Win,
+            Lose
+        }
+        private static double _indexGameid = 1000;
+        public int Raiting { get; }
+        public int IndexGame { get; }
+        public string Oponent { get; }
+        public ResultGame Result { get; }
+        
+        public Game(int raiting, string opponentName, ResultGame result)
+        {
+            IndexGame = (int)_indexGameid;
+            _indexGameid+=0.5;
+            Raiting = raiting;
+            Result = result;
+            Oponent = opponentName;
+        }
+    }
+}
